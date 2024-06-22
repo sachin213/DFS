e38662e91ac51cb113f99781eae21ef0a3e9a6b0
sachin dave <davesachin26@gmail.com>
2024-06-22 22:05:13 +0530

    Reverse the string word

--- src/ReverseStrWord.java
+++ src/ReverseStrWord.java
@@ -1,6 +1,9 @@
 public class ReverseStrWord {
     public static void main(String[] args) {
         String s = "java is a object oriented programming";
-
+        String strArr [] = s.split(" ");
+        for(int i = strArr.length-1;i>=0;i--){
+            System.out.print(strArr[i]+ " ");
+        }
     }
 }
